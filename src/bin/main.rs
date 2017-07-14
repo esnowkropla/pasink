75009d14c564f8527a90be3ab5a62d0713245234
Elliot Snow-Kropla <esnowkropla@gmail.com>
2017-07-14 16:32:13 -0400

    Play around with the windows api

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,9 +1,31 @@
+#[cfg(windows)]
+extern crate winapi;
+extern crate user32;
+
 extern crate pasink;
 
 use std::env;
 use pasink::{server, client};
 
+#[cfg(windows)]
+fn open_window() {
+    use std::ffi::OsStr;
+    use std::iter::once;
+    use std::os::windows::ffi::OsStrExt;
+    use std::ptr::null_mut;
+    use winapi::winuser::MB_OK;
+    use user32::MessageBoxW;
+    let wide: Vec<u16> = OsStr::new("hello").encode_wide().chain(once(0)).collect();
+    let ret = unsafe {
+        MessageBoxW(null_mut(), wide.as_ptr(), wide.as_ptr(), MB_OK);
+    };
+}
+
+#[cfg(not(windows))]
+fn open_window() {}
+
 fn main() {
+    open_window();
     let args: Vec<String> = env::args().collect();
     if args[1] == "server" {
         server(args[2].as_str());
